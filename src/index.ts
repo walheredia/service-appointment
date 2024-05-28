f36641af1be69fbda2900c70a883f738571c0d58
Walter-Heredia <wal_dh@hotmail.com>
2024-05-28 18:19:47 -0300

    add db connection

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,25 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import sequelize from './config/database';
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Hello World!');
 });
 
+// Test the database connection
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err: any) => {
+    console.error('Unable to connect to the database:', err);
+  });
+
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
